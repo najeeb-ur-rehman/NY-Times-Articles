2a1968b5024504dd03ae97031074f51d908e5837
Najeeb <najeeb.rehman@digitify.com>
2022-10-24 15:35:55 +0500

    Organize file structure

---
+++ NY Times Articles/Scenes/ArticlesList/ViewModels/ArticlesListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArticlesListViewModel.swift
+//  NY Times Articles
+//
+//  Created by Najeeb on 24/10/2022.
+//
+
+import Foundation
---
+++ NY Times Articles/Scenes/ArticlesList/Views/ArticlesListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArticlesListViewController.swift
+//  NY Times Articles
+//
+//  Created by Najeeb on 24/10/2022.
+//
+
+import Foundation
+import UIKit
+
+class ArticlesListViewController: UIViewController {
+    
+}
